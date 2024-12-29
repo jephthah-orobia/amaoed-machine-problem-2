efab9e4717070e553f600603e7e38976b9548d87
Jephthah Orobia <115846010+jephthah-orobia@users.noreply.github.com>
2024-12-29 09:51:28 +0000

    implement machine problem 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,70 @@
 /* ****************************************************************
- * @author: <your name here>
- * @app name: <your application name here>
- * @app desc: <describe the purpose of your application here>
- * @history:
- *  - <yyyy/mm/dd> | <name>
- *      -- <description>
- *      -- <description>
- *      -- <description>
- *  - <yyyy/mm/dd> | <name>
- *      -- <description>
- *      -- <description>
- *      -- <description>
+ * @author: Jephthah M. Orobia
+ * @app name: Seat Keeper
+ * @app desc: This app reserves seat.
+ *            This app is a partial requirement for Com Prog 1
+ *            "Machine Problem 2"
  * ****************************************************************/
 
 #include <cstdio>
 #include <cstdlib>
+#include <limits>
 #include <iostream>
 #include "_pause.h"
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////
-//                               NOTE
-// This is your program entry point. Your main logic is placed
-// inside this function. You may add your functions before this
-// "main()", or after this "main()" provided you added reference
-// before this "main()" function.
-//////////////////////////////////////////////////////////////////
-
 int main()
 {
-  // ************************** TO DO **************************
-  // Place your code logic after this comment line
-  // ***********************************************************
+  int seats[5][7], n;
 
-  cout << "Hello World!" << endl;
+  for (int s = 0; s < 35; s++)
+  {
+    seats[s / 7][s % 7] = s + 1;
+  }
 
-  // ********************** DO NOT CHANGE **********************
-  // Print a new line and ask user for any key before exiting
-  // ***********************************************************
+  while (true)
+  {
+
+    for (int row = 0; row < 5; row++)
+    {
+      for (int col = 0; col < 7; col++)
+      {
+        cout << seats[row][col] << "\t";
+      }
+      cout << endl;
+    }
+
+    cout << endl
+         << "Enter seat number to reserve: ";
+    cin >> n;
+    system("clear");
+    cout << endl;
+    if (!cin.fail() && n > 0 && n < 36)
+    {
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      int r = (n - 1) / 7, c = (n - 1) % 7;
+      if (seats[r][c] == 0)
+      {
+        cout << "<< Seat #" << n << " is taken >>";
+      }
+      else
+      {
+        seats[r][c] = 0;
+        cout << "<< Seat #" << n << " is successfully reserved. >>";
+      }
+    }
+    else
+    {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "!!! Invalid seat number. Try again. !!!";
+    }
+
+    cout << endl
+         << endl;
+  }
+
   _pause();
   return EXIT_SUCCESS;
 }
